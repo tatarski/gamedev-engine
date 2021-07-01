cb08b9028f2ed07cf26d8db2d6cfd499ca41a84d
tatarski <tatarski.dim@gmail.com>
2021-07-01 20:53:32 +0300

    Framework from last year

---
+++ game.js
@@ -0,0 +1,25 @@
+// 1. Suzdavame promenlivi
+let myX = 0,
+    myY = 0;
+
+function update() {
+    // 2. Kodut tuk se izpulnqva 100 puti v sekunda
+    myX = myX + (mouseX - myX) / 10;
+    myY = myY + (mouseY - myY) / 10;
+}
+
+function draw() {
+    // 3. Tuk naprogramirai kakvo da se risuva
+    drawImage(backDesert, 0, 0, 800, 600);
+    drawImage(femaleAction, myX, myY, 60, 80);
+}
+
+function keyup(key) {
+    // Pechatai koda na natisnatiq klavish
+    console.log("Pressed", key);
+}
+
+function mouseup() {
+    // Pri klik - pokaji koordinatite na mishkata
+    console.log("Mouse clicked at", mouseX, mouseY);
+}
