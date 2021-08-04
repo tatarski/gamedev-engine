e11914450b5c0bb512995325db9005787baa02b3
tatarski <tatarski@gmail.com>
2021-08-04 22:46:08 +0300

    Final changes

--- game.js
+++ game.js
@@ -1,31 +1,26 @@
-// 0. Suzdavame promenlivi
-let myX, myY;
-
+// Suzdavame promenlivi
+let myX, myY, gosho = [];
 function init() {
-    // 1. Kodut tuk se izpulnqva vednuj v nachaloto
+    // Kodut tuk se izpulnqva vednuj v nachaloto
     myX = 300;
     myY = 300;
 }
-
 function update() {
-    // 2. Kodut tuk se izpulnqva (okolo) 100 puti v sekunda
+    // Kodut tuk se izpulnqva (okolo) 100 puti v sekunda
     myX = myX + (mouseX - myX) / 10;
     myY = myY + (mouseY - myY) / 10;
 }
-
 function draw() {
-    // 3. Tuk naprogramirai kakvo da se risuva
+    // Tuk naprogramirai kakvo da se risuva
     drawImage(backDesert, 0, 0, 800, 600);
     drawImage(femaleAction, myX, myY, 60, 80);
-    context.fillText("GOSHO", 50, 50);
 }
-
+function mouseup() {
+    // Pri klik s lqv buton - pokaji koordinatite na mishkata
+    console.log("Mouse clicked at", mouseX, mouseY);
+}
 function keyup(key) {
-    // 4. Pechatai koda na natisnatiq klavish
+    // Pechatai koda na natisnatiq klavish
     console.log("Pressed", key);
 }
 
-function mouseup() {
-    // 5. Pri klik s lqv buton - pokaji koordinatite na mishkata
-    console.log("Mouse clicked at", mouseX, mouseY);
-}
