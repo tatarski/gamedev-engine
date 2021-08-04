49e6bcb059581c46df61e63e6aa5def46feb4daa
tatarski <tatarski@gmail.com>
2021-08-04 21:53:05 +0300

    Refactor, all exeecution begins on body load

--- game.js
+++ game.js
@@ -17,6 +17,7 @@ function draw() {
     // 3. Tuk naprogramirai kakvo da se risuva
     drawImage(backDesert, 0, 0, 800, 600);
     drawImage(femaleAction, myX, myY, 60, 80);
+    context.fillText("GOSHO", 50, 50);
 }
 
 function keyup(key) {
